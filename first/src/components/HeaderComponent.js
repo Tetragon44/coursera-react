1026c2dacf81400ef8b0fb2256d3bf8d27ba3244
Tetragon <tetragon444@gmail.com>
2020-05-07 12:23:33 +0300

    Redux

--- first/src/components/HeaderComponent.js
+++ first/src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import{ Navbar, NavbarBrand,Nav, NavbarToggler, NavItem, Collapse,  Jumbotron} from 'reactstrap';
+import{ Navbar, NavbarBrand,Nav, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalBody, ModalHeader, FormGroup, Label, Form, Input} from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 
@@ -7,10 +7,14 @@ class Header extends Component{
   constructor(props) {
     super(props);
 
-    this.toggleNav = this.toggleNav.bind(this);
+    
     this.state = {
-      isNavOpen: false
+      isNavOpen: false,
+      isModalOpen: false
     };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav() {
@@ -18,6 +22,18 @@ class Header extends Component{
       isNavOpen: !this.state.isNavOpen
     });
   }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+    event.preventDefault();
+
+}
   render(){
     return(
       <>
@@ -51,6 +67,13 @@ class Header extends Component{
                   </NavLink>
                 </NavItem>
               </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"> Login</span>
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
             
           </div>
@@ -69,6 +92,32 @@ class Header extends Component{
           </div>
 
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                  <Input type="text" id="username" name="username"
+                    innerRef={(input) => this.username = input} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                    <Input type="password" id="password" name="password"
+                        innerRef={(input) => this.password = input}  />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember"
+                      innerRef={(input) => this.remember = input}  />
+                          Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Login</Button>
+              
+            </Form>
+          </ModalBody>
+        </Modal>
 
       </>
     );
--- first/src/components/MainComponent.js
+++ first/src/components/MainComponent.js
@@ -6,42 +6,43 @@ import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import DishDetailComponent from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import { connect} from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
+
 
 
 class Main extends Component{
   constructor(props){
     super(props);
-    this.state={
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-
-    };
-    
   }
   
+  
+
   render(){
     const HomePage= ()=>{
       return(
-        <Home dish={this.state.dishes.filter
+        <Home dish={this.props.dishes.filter
           ((dish)=>dish.featured)[0]} 
-          promotion={this.state.promotions.filter
+          promotion={this.props.promotions.filter
             ((promotion)=>promotion.featured)[0]}
-            leader={this.state.leaders.filter
+            leader={this.props.leaders.filter
               ((leader)=>leader.featured)[0]}
               />
       );
     }
     const DishWithId = ({match}) => {
       return(
-          <DishDetailComponent dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+          <DishDetailComponent dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
     return (
@@ -51,10 +52,10 @@ class Main extends Component{
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" 
               component={()=>
-              <Menu dishes={this.state.dishes} />} />
+              <Menu dishes={this.props.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contact" component={Contact} />
-          <Route exact path="/about" component={()=><About leaders={this.state.leaders}/>} />
+          <Route exact path="/about" component={()=><About leaders={this.props.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
@@ -66,4 +67,4 @@ class Main extends Component{
   
 }
 
-export default Main;
+export default (connect(mapStateToProps)(Main));
